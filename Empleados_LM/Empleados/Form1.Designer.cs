65529f6da7d898c2dcbd2f2ebc9d19ecb4435b3c
Fer516 <110191651+Fer516@users.noreply.github.com>
2022-10-08 17:28:01 -0600

    Add files via upload

---
+++ Empleados_LM/Empleados/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿
+namespace Empleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelDUI = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.labelRegistro = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtAFP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btModificar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dui = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Salario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AFP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelNombre.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelNombre.Location = new System.Drawing.Point(118, 47);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(44, 13);
+            this.labelNombre.TabIndex = 1;
+            this.labelNombre.Text = "Nombre";
+            this.labelNombre.Click += new System.EventHandler(this.labelNombre_Click);
+            // 
+            // labelDUI
+            // 
+            this.labelDUI.AutoSize = true;
+            this.labelDUI.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelDUI.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelDUI.Location = new System.Drawing.Point(118, 85);
+            this.labelDUI.Name = "labelDUI";
+            this.labelDUI.Size = new System.Drawing.Size(26, 13);
+            this.labelDUI.TabIndex = 2;
+            this.labelDUI.Text = "DUI";
+            this.labelDUI.Click += new System.EventHandler(this.labelDUI_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(204, 47);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.Location = new System.Drawing.Point(204, 85);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.Size = new System.Drawing.Size(100, 20);
+            this.txtDUI.TabIndex = 5;
+            this.txtDUI.TextChanged += new System.EventHandler(this.txtDUI_TextChanged);
+            // 
+            // labelRegistro
+            // 
+            this.labelRegistro.AutoSize = true;
+            this.labelRegistro.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelRegistro.Location = new System.Drawing.Point(283, 275);
+            this.labelRegistro.Name = "labelRegistro";
+            this.labelRegistro.Size = new System.Drawing.Size(0, 13);
+            this.labelRegistro.TabIndex = 7;
+            this.labelRegistro.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // txtAFP
+            // 
+            this.txtAFP.Location = new System.Drawing.Point(204, 162);
+            this.txtAFP.Name = "txtAFP";
+            this.txtAFP.Size = new System.Drawing.Size(100, 20);
+            this.txtAFP.TabIndex = 9;
+            this.txtAFP.TextChanged += new System.EventHandler(this.txtAFP_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(118, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Salario";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(204, 121);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 20);
+            this.txtSalario.TabIndex = 11;
+            this.txtSalario.TextChanged += new System.EventHandler(this.txtSalario_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(118, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "AFP";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(121, 259);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(116, 44);
+            this.btnInsertar.TabIndex = 13;
+            this.btnInsertar.Text = "Insertar Registro";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(469, 37);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 14;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(563, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(762, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Ver todo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(469, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btModificar
+            // 
+            this.btModificar.Location = new System.Drawing.Point(626, 321);
+            this.btModificar.Name = "btModificar";
+            this.btModificar.Size = new System.Drawing.Size(75, 23);
+            this.btModificar.TabIndex = 18;
+            this.btModificar.Text = "Modificar";
+            this.btModificar.UseVisualStyleBackColor = true;
+            this.btModificar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nombre,
+            this.Dui,
+            this.Salario,
+            this.AFP});
+            this.dataGridView1.Location = new System.Drawing.Point(327, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(548, 130);
+            this.dataGridView1.TabIndex = 19;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "ID";
+            this.Id.Name = "Id";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Dui
+            // 
+            this.Dui.HeaderText = "DUI";
+            this.Dui.Name = "Dui";
+            // 
+            // Salario
+            // 
+            this.Salario.HeaderText = "Salario";
+            this.Salario.Name = "Salario";
+            // 
+            // AFP
+            // 
+            this.AFP.HeaderText = "AFP";
+            this.AFP.Name = "AFP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(887, 460);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btModificar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAFP);
+            this.Controls.Add(this.labelRegistro);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.labelDUI);
+            this.Controls.Add(this.labelNombre);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Registro de empleados";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelDUI;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.Label labelRegistro;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAFP;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btModificar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dui;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Salario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AFP;
+    }
+}
+
